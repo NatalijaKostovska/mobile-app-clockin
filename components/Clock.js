ff158dcebcb1989df8700ce3c9a03c234dcc88b2
Dime Atanasov <dimeatanasov11@gmail.com>
2025-02-22 22:53:25 +0100

    feat: location condition

--- components/Clock.js
+++ components/Clock.js
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect } from "react";
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import { View, Text, StyleSheet, TouchableOpacity, Alert } from "react-native";
 import { AuthContext } from "../context/AuthContext";
 import {
   collection,
@@ -9,6 +9,21 @@ import {
   updateDoc,
 } from "firebase/firestore";
 import { db } from "../firebase/firebaseConfig";
+
+import * as Location from "expo-location";
+
+const targetLocation = {
+  latitude: 37.332329,
+  longitude: -122.031191,
+};
+
+const calculateDistance = (lat1, lon1, lat2, lon2) => {
+  const latDiff = (lat2 - lat1) * 111320;
+  const lonDiff =
+    (lon2 - lon1) * 111320 * Math.cos(((lat1 + lat2) / 2) * (Math.PI / 180));
+
+  return Math.sqrt(latDiff ** 2 + lonDiff ** 2);
+};
 
 const Clock = () => {
   const { authState, setAuthState } = useContext(AuthContext);
@@ -42,26 +57,58 @@ const Clock = () => {
     }
   };
 
+  const getLocation = async () => {
+    let { status } = await Location.requestForegroundPermissionsAsync();
+    if (status !== "granted") {
+      Alert.alert(
+        "Permission denied",
+        "Location access is required for check-in."
+      );
+      return;
+    }
+    let loc = await Location.getCurrentPositionAsync();
+
+    const distance = calculateDistance(
+      loc.coords.latitude,
+      loc.coords.longitude,
+      targetLocation.latitude,
+      targetLocation.longitude
+    );
+
+    return distance <= 10;
+  };
+
   const handleClockIn = async () => {
     const userId = authState.user.id;
 
     const clockInTime = Timestamp.fromDate(new Date());
     let docRef = doc(collection(db, "user-clocked"));
-    try {
-      await setDoc(
-        docRef,
-        {
-          userId: userId,
-          startTime: clockInTime,
-        },
-        { merge: true }
+    const currentLocation = getLocation();
+
+    if (!currentLocation) {
+      try {
+        await setDoc(
+          docRef,
+          {
+            userId: userId,
+            startTime: clockInTime,
+          },
+          { merge: true }
+        );
+
+        const docId = docRef.id;
+        await saveClockInInfo(docId);
+        Alert.alert("Clocked", "Enjoy on your shift!");
+      } catch (error) {
+        console.error("Error clocking in:", error);
+        throw error;
+      }
+    } else {
+      Alert.alert(
+        "Not clocked",
+        "Sorry but you are still not at your working place!"
       );
-    } catch (error) {
-      console.error("Error clocking in:", error);
-      throw error;
     }
-    const docId = docRef.id;
-    await saveClockInInfo(docId);
   };
 
   const handleClockOut = async () => {
